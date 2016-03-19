2815c72b3693a5587382f4b9cb745e0d39bf6312
wtheisen <wtheisen@nd.edu>
2016-03-19 15:49:41 -0400

    Added files via upload

---
+++ Move.h
@@ -0,0 +1,21 @@
+#include <iostream>
+//#include "chess.crap"
+
+#ifndef Move_H
+#define Move_H
+
+class Move
+{
+	public:
+		Move();
+	
+		Move(Piece piece, int startRow, int startCol, int endRow, int endCol);
+	
+	private: 
+		Piece piece;
+		int startRow;
+		int startCol;
+		int endRow;
+		int endCol;
+		double value;	
+}
